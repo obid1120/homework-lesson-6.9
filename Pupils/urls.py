83f48347141d73d38cbc8dc394018c6290e38781
obid1120 <yoqubovobidjon99@gmail.com>
2024-03-14 16:19:21 +0500

    Advanced some files

--- Pupils/urls.py
+++ Pupils/urls.py
@@ -4,9 +4,9 @@ from .views import pupils, pupil1, pupil2, pupil3, pupil4, pupil5
 
 urlpatterns = [
     path('', pupils, name='pupils'),
-    path('1/', pupil1, name='pupil1'),
-    path('2/', pupil2, name='pupil2'),
-    path('3/', pupil3, name='pupil3'),
-    path('4/', pupil4, name='pupil4'),
-    path('5/', pupil5, name='pupil5'),
+    path('1/', pupil1, name='1'),
+    path('2/', pupil2, name='2'),
+    path('3/', pupil3, name='3'),
+    path('4/', pupil4, name='4'),
+    path('5/', pupil5, name='5'),
 ]
--- Pupils/views.py
+++ Pupils/views.py
@@ -1,29 +1,33 @@
 from django.shortcuts import render
 
 pupils_data = {
-    1: ['Yusufjon Muhammadov', ]
+    1: 'Yusufjon Muhammadov',
+    2: 'Nurbek Doniyorov',
+    3: 'Boburmirzo Muhammadov',
+    4: 'Fazliddin Asomov',
+    5: 'Muhammadjon Ibrohimov'
 }
 
 
 def pupils(request):
-    return render(request, 'pupils.html', {'hello': []})
+    return render(request, 'pupils.html', {'pupils_ls': pupils_data})
 
 
 def pupil1(request):
-    return render(request, 'pupil1.html', {'hello': []})
+    return render(request, 'pupil1.html')
 
 
 def pupil2(request):
-    return render(request, 'pupil2.html', {'hello': []})
+    return render(request, 'pupil2.html')
 
 
 def pupil3(request):
-    return render(request, 'pupil3.html', {'hello': []})
+    return render(request, 'pupil3.html')
 
 
 def pupil4(request):
-    return render(request, 'pupil4.html', {'hello': []})
+    return render(request, 'pupil4.html')
 
 
 def pupil5(request):
-    return render(request, 'pupil5.html', {'hello': []})
+    return render(request, 'pupil5.html')
